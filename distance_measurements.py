ce2dccbf7d4d99484e9ffc5cde6575c44d1366d2
Konrad Drees <konrad.drees@posteo.de>
2022-04-03 12:36:22 +0200

    Update distance_measurements.py

--- distance_measurements.py
+++ distance_measurements.py
@@ -1,16 +1,22 @@
 import numpy as np
 
 
+# alpha = 2 for euclid, = 1 for manhattan, alpha -> infinity for tschebyschew distance
 def minkowski_distance(x, y, alpha):
-    distance = 0
-    for x_i in x:
-        for y_i in y:
-            distance += abs(x_i-y_i)**alpha
-    distance**(1/alpha)
+    array1, array2 = np.array(x), np.array(y)
+    distance = sum(abs(array1-array2)**alpha)**(1/alpha)
     return distance
+
+
+# cosinus similarity
+def cos_sim(x, y):
+    array1, array2 = np.array(x), np.array(y)
+    similarity = (np.dot(array1, array2))/(np.sqrt(sum(array1**2))*np.sqrt(sum(array2**2)))
+    return similarity
 
 
 x = (2.6, 3)
 y = (2.6, 2.3)
 
 print(minkowski_distance(x, y, 2))
+print(cos_sim(x, y))
