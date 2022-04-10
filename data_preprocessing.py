45446588375ac42f71aeb7fdc9d9edbcaa7bee9b
Konrad Drees <konrad.drees@posteo.de>
2022-04-10 22:37:13 +0200

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,28 +1,30 @@
 import numpy as np
 
 
-def arithmetic_mean(x,y):
+def arithmetic_mean(x, y):
     return (x+y)/2
 
-def smooth_mean(x,y):
-    return x - arithmetic_mean(x,y)
 
-#iterative solution for a wavelet_transformations
+def smooth_mean(x, y):
+    return x - arithmetic_mean(x, y)
+
+
+# iterative solution for a wavelet_transformations
 def discrete_wavelet_transformation(data, threshold):
     step_data = list(data)
     if len(step_data) % 2 != 0:
         step_data.append(0)
     length = len(step_data)
     times = int(np.log2(length))
-    solution = np.zeros((times+1,length))
+    solution = np.zeros((times+1, length))
     solution[0] = step_data
     for i in range(0,times):
-        for j in range(0,2 ** (times-i),2):
-            solution[i+1][int(j/2)] = (arithmetic_mean(solution[i][j],solution[i][j+1]))
+        for j in range(0,2 ** (times-i), 2):
+            solution[i+1][int(j/2)] = (arithmetic_mean(solution[i][j], solution[i][j+1]))
         for j in range(0, 2 ** (times - i), 2):
-            for k in range(i,times):
+            for k in range(i, times):
                 smooth = smooth_mean(solution[i][j], solution[i][j + 1])
-                solution[k+1][int (2 ** (times-i- 1)  + j/2)] = smooth if threshold <= smooth else 0
+                solution[k+1][int(2 ** (times-i-1) + j/2)] = smooth if threshold <= smooth else 0
     return np.array(solution)
 
 
